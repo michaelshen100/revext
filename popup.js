590d2b87d1848bd132c14fde675acdf2051097a4
kavinahsu <36987178+kavinahsu@users.noreply.github.com>
2018-10-21 00:41:23 -0500

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,16 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+function loadUrls(){
+    //
+}
+
+function saceUrls(){
+    //
+}
+
+document.getElementById('button').addEventListener('click', loadUrls);
+document.getElementById('button').addEventListener('click', saveUrls);
